a831a0a0747ab79cf8365959d4034ddc7c48e492
vx-dev <86169807+vx-dev@users.noreply.github.com>
2021-07-19 15:35:17 -0500

    Add auto spoof & back function(s)

--- macster.py
+++ macster.py
@@ -16,7 +16,7 @@ two ='\'\"'
 cmd = os.system
 init()
 def initialize():
-    cmd('echo off')
+    cmd('echo on')
     logo()
     print(green, 'Checking if ADB Daemon is Running...')
     time.sleep(0.3)
@@ -118,6 +118,11 @@ def lookup():
         print("MAC Address: "+mac)
         for i in 'name', 'address':
             print(f"{i.capitalize()}:{r['result'][0][i]}")
+        c = input('Press Any Key to Continue')
+        if c == 'a':
+            main()
+        else:
+            main()
 def lookup1():
     logo()
     print('Enter MAC Address')
@@ -153,13 +158,18 @@ def spoofmenu():
     print("""
         [""" + cyan + """1""" + reset + """] Custom Mac Address
         [""" + cyan + """2""" + reset + """] Preset Mac Address
-
+        [""" + cyan + """3""" + reset + """] Automatic (Spoofs Your Mac Address Every 15 Minutes)
+        [""" + cyan + """4""" + reset + """] Go Back
         """)
     mode = input('Mode: ')
     if mode == '1':
         customspoof()
     elif mode == '2':
         presetspoof()
+    elif mode == '3':
+        autospoof()
+    elif mode == '4':
+        main()
 def customspoof():
     try:
         logo()
@@ -251,4 +261,106 @@ def presetspoof():
             main()
         except:
             error()
+def autospoof():
+    mac1 = "00:50:c2:76:96:b9"
+    mac2 = "f0:de:f1:89:6e:25"
+    mac3 = "98:ae:71:6f:1f:a9"
+    mac4 = "d8:86:0b:2a:0d:25"
+    mac5 = "00:50:8b:71:bd:57"
+    mac6 = "70:b3:d5:ad:2f:6d"
+    mac7 = "00:01:6f:c1:8c:9d"
+    mac8 = "00:0c:7f:f5:74:c7"
+    mac9 = "00:e0:88:f5:d9:00"
+    mac10 = "44:7e:95:db:be:89"
+    mac11 = "00:17:e5:fb:42:f3"
+    mac12 = "28:31:66:ad:d9:2b"
+    mac13 = "00:90:93:5d:c6:1d"
+    mac14 = "ac:a4:6e:69:2c:c6"
+    mac15 = "00:19:d6:a7:a1:9e"
+    spoofed = "Spoofed MAC Address!"
+    loopstart = "~ Start of Loop ~"
+    loopend = "~ End of Loop ~"
+    try:
+        logo()
+        print(orange, loopstart)
+        cmd(off)
+        cmd(one+mac1+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac2+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac3+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac4+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac5+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac6+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac7+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac8+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac9+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac10+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac11+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac12+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac13+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac14+two)
+        cmd(on)
+        print(green, spoofed)
+        time.sleep(4.8)
+        cmd(off)
+        cmd(one+mac15+two)
+        cmd(on)
+        print(green, spoofed)
+        print(orange, loopend)
+        time.sleep(4.8)
+        autospoof()
+    except:
+        cmd(on)
+        error()
 initialize()
